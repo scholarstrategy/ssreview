e68cb3ef0116ea536ca82a0b035415e55b875266
pranjalsingi <pranjalsingi710@gmail.com>
2017-08-22 02:46:34 -0400

    changed filenames in route.js

--- app/route.js
+++ app/route.js
@@ -4,11 +4,11 @@ app.config(function($routeProvider){
 	console.log('coming inside though');
 	$routeProvider
 	.when("/", {
-		templateUrl : 'file:///home/pranjal/Projects/scholar/app/components/home/homeView.html', 
+		templateUrl : 'components/home/homeView.html', 
 		controller	: 'homeCtrl'
 	})
 	.when("/students", {
-		templateUrl	: 'file:///home/pranjal/Projects/scholar/app/components/students/studentView.html',
+		templateUrl	: 'components/students/studentView.html',
 		controller 	: 'studentCtrl'
 	})
 	// .when("/view/:student_id", {
